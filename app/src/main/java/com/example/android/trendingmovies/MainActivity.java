fe672473647c3f6b76aa4729bbc808507b63c342
Sushant Mathur <sushant.post@gmail.com>
2016-08-18 16:42:50 +0530

    Updated gradle and created a new main activity supporting main activity.

--- app/src/main/java/com/example/android/trendingmovies/MainActivity.java
+++ app/src/main/java/com/example/android/trendingmovies/MainActivity.java
@@ -1,23 +1,24 @@
 package com.example.android.trendingmovies;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity implements MovieInformation.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 
@@ -43,5 +44,10 @@ public class MainActivity extends AppCompatActivity {
         }*/
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
