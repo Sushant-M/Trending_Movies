fe31a79be2119412bc190cdcdbf144eb5cdb764e
Sushant Mathur <sushant.post@gmail.com>
2016-08-19 00:27:24 +0530

    DONE. Saving state remaining.

--- app/src/main/java/com/example/android/trendingmovies/MovieInformation.java
+++ app/src/main/java/com/example/android/trendingmovies/MovieInformation.java
@@ -4,6 +4,7 @@ import android.content.ActivityNotFoundException;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
@@ -47,6 +48,9 @@ public class MovieInformation extends Fragment {
     String youTubeURL = null;
     String tempdata = null;
     String check = null;
+    String MovieID = null;
+    private boolean toggle_bool = false;
+    ContentValues contentValues =  new ContentValues();
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -95,10 +99,29 @@ public class MovieInformation extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_movie_information, container, false);
         Button launch = (Button)view.findViewById(R.id.trailerbutton);
+        final Button fav = (Button)view.findViewById(R.id.favorite);
+
+        fav.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                toggle_bool = !toggle_bool;
+                if(toggle_bool == true){
+                    fav.setText("In Favorite");
+                    //add entry to database
+                    new AddEntry().execute(contentValues);
+
+                }else if(toggle_bool == false){
+                    fav.setText("Not in Favorite");
+                    //delete entry from database
+                    new DeleteEntry().execute(MovieID);
+                }
+            }
+        });
+
+
         launch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d(TAG,"SOME SHIT");
                     try {
                         Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube.com:" + tempdata));
                         startActivity(intent);
@@ -113,6 +136,35 @@ public class MovieInformation extends Fragment {
         });
 
         return view;
+    }
+
+    public class DeleteEntry extends AsyncTask<String,Void,Void>{
+        @Override
+        protected Void doInBackground(String... params) {
+            SQLiteOpenHelper sqLiteOpenHelper = new MovieDatabase(getContext());
+            SQLiteDatabase db = sqLiteOpenHelper.getWritableDatabase();
+            String TABLENAME = MovieContract.TABLE_NAME;
+            String selection = MovieContract.COLUMN_MOVIE_ID + " LIKE ?";
+            String[] selectionargs = { params[0] };
+            db.delete(TABLENAME,selection,selectionargs);
+            return null;
+        }
+    }
+
+    public class AddEntry extends AsyncTask<ContentValues,Void,Long>{
+
+        @Override
+        protected Long doInBackground(ContentValues... params) {
+            SQLiteOpenHelper mHelper = new MovieDatabase(getContext());
+            SQLiteDatabase db = mHelper.getWritableDatabase();
+            long number = db.insert(
+                    MovieContract.TABLE_NAME,
+                    null,
+                    params[0]
+            );
+            Log.d(TAG,Long.toString(number));
+            return number;
+        }
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -138,7 +190,7 @@ public class MovieInformation extends Fragment {
         String Movie_OverView = bundle.getString("overview");
         String Movie_Release = bundle.getString("release");
         String Movie_Rating = bundle.getString("rating");
-        String MovieID = bundle.getString("id");
+        MovieID = bundle.getString("id");
 
         check = bundle.getString("favorite");
         String toCompare = new String("false");
@@ -172,6 +224,27 @@ public class MovieInformation extends Fragment {
         Picasso.with(context).load(url).into(posterimg);
 
         new GetYoutubeLink().execute(MovieID);
+
+        contentValues.put(MovieContract.COLUMN_MOVIE_NAME,Title);
+        contentValues.put(MovieContract.COLUMN_MOVIE_POSTER,Image_Path);
+        contentValues.put(MovieContract.COLUMN_MOVIE_RATING,Movie_Rating);
+        // to be added later contentValues.put(MovieContract.COLUMN_MOVIE_REVIEW,"review goes here");
+        contentValues.put(MovieContract.COLUMN_SYNOPSIS,Movie_OverView);
+        //to be added later contentValues.put(MovieContract.COLUMN_YOUTUBE_LINK,tempdata);
+        contentValues.put(MovieContract.COLUMN_MOVIE_RELEASE,Movie_Release);
+        contentValues.put(MovieContract.COLUMN_MOVIE_ID,MovieID);
+
+        String result= null;
+
+        new CheckIfDataIsPresent().execute(MovieID,null,result);
+
+        Button toggle = (Button)getView().findViewById(R.id.favorite);
+        toggle.setText(result);
+
+
+        String rev = null;
+
+        new GetReview().execute(MovieID,null,rev);
     }
 
 
@@ -282,12 +355,124 @@ public class MovieInformation extends Fragment {
         }
     }
 
+    public class CheckIfDataIsPresent extends AsyncTask<String,Void,String>{
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Button toggle = (Button)getView().findViewById(R.id.favorite);
+            toggle.setText(s);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            SQLiteOpenHelper mHelper = new MovieDatabase(getContext());
+            SQLiteDatabase db = mHelper.getWritableDatabase();
+            String query = "SELECT * FROM " + MovieContract.TABLE_NAME + " WHERE " + MovieContract.COLUMN_MOVIE_ID + " = " + params[0];
+            Cursor cursor = db.rawQuery(query,null);
+            if(cursor.getCount()<=0){
+                return "Not in Favorite";
+            }
+            cursor.close();
+            return "In Favorite";
+        }
+    }
+
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
 
+    public class GetReview extends AsyncTask<String,Void,String>{
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            TextView movie_review = (TextView)getView().findViewById(R.id.review);
+            movie_review.setText(s);
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            String reviewJSON = null;
+            try{
+                final String BASEURL = "http://api.themoviedb.org/3";
+                final String MOVIES = "movie";
+                final String REVIEW = "reviews";
+                final String APIPARAM = "api_key";
+                final String APIKEY = "fc53fdb027975aaacc7595aeb259107d";
+                String movID = params[0];
+
+                Uri builtUri = Uri.parse(BASEURL).buildUpon()
+                        .appendEncodedPath(MOVIES)
+                        .appendEncodedPath(movID)
+                        .appendEncodedPath(REVIEW)
+                        .appendQueryParameter(APIPARAM,APIKEY)
+                        .build();
+
+                URL url = new URL(builtUri.toString());
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                reviewJSON = buffer.toString();
+            }catch (IOException e){
+                e.printStackTrace();
+            }finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e(TAG, "Error closing stream", e);
+                    }
+                }
+            }
+            try{
+                String review = parseReview(reviewJSON);
+                return review;
+            }catch (JSONException e){
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+    public String parseReview(String json) throws JSONException{
+        String list = "results";
+        String id = "content";
+        JSONObject moviesOBJ = new JSONObject(json);
+        JSONArray movieArr = moviesOBJ.getJSONArray(list);
+        int i = 0;
+        JSONObject obj = movieArr.getJSONObject(i);
+        String review = obj.getString(id);
+        return review;
+    }
     /**
      * This interface must be implemented by activities that contain this
      * fragment to allow an interaction in this fragment to be communicated
